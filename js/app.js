4b16e5e381d801472568974888d2f471c53eaf48
Nicholas Humes <njhumes@gmail.com>
2018-12-12 21:50:41 -0700

    Commit 9 - Project Euler Problem 1 answered

--- js/app.js
+++ js/app.js
@@ -108,4 +108,10 @@ for (let i = 0; i < kristynsCloset.length; i++) {
 for (let i = 0; i < thomsCloset.length; i++){
     console.log(thomsCloset[i]);
 }
-
+let sum = 0;
+for (let i = 1; i < 1001; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+        sum += i;
+    }
+} 
+console.log(sum);
